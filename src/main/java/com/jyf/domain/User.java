32b6e447216289b0761b7fcf6671a746cd79b5b0
jyf <798260701@qq.com>
2019-11-17 21:30:56 +0800

    修改服务器和地址

---
+++ src/main/java/com/jyf/domain/User.java
@@ -0,0 +1,60 @@
+package com.jyf.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Date;
+//@JsonIgnoreProperties()
+public class User {
+
+    private String name;
+    @JsonIgnore
+    private String password;
+    private int age;
+    @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss a", locale="zh", timezone="GMT+8")
+    private Date birthday;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
